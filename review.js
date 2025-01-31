66390a123316a7c81783c7bc3d560e6daf1244fc
sahitipepakayala <163521030+sahitipepakayala@users.noreply.github.com>
2025-01-31 08:14:28 +0530

    Add files via upload
    model folder

--- review.js
+++ review.js
@@ -1,28 +1,23 @@
-const review=require("../models/review");
-const listing=require("../models/listing");
+const mongoose=require("mongoose");
 
-module.exports.newreview=async (req,res)=>{
-    let listing1= await listing.findById(req.params.id);
-
-    if (!listing1) {
-        console.log(listing1)
-        throw new ExpressError(404, "not found");
+let reviewschema=new mongoose.Schema({
+  
+    content:{type:String,
+        required:true,
+    },
+    rating:{
+        type:Number,
+        min:1,
+        max:5,
+    },
+    created_on:{
+        type:Date,
+        default:Date.now(),
+    },
+    author:{
+        type:mongoose.Schema.Types.ObjectId,
+            ref:"user",
     }
-    let review1 = new review(req.body.review); // Assuming you have a `Review` model
-    review1.author=req.user._id;
-    console.log( review1.author)
-    await review1.save();
-    listing1.reviews.push(review1); // Push the review to the reviews array
-    await listing1.save();
-    req.flash("rv","added the review");
-    res.redirect(`/listings/${listing1._id}`);
- 
-}
+})
 
-module.exports.deletereview=async(req,res)=>{
-    let {id,reviewId}=req.params;
-    await listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-   await review.findByIdAndDelete(reviewId);
-   req.flash("dt","deleted the review");
-   res.redirect(`/listings/${id}`);
-}
+module.exports=new mongoose.model("Review",reviewschema);
--- user.js
+++ user.js
@@ -1,45 +1,15 @@
-const user = require("../models/user");
+const mongoose=require("mongoose");
+const passportlocalmongoose=require("passport-local-mongoose");
 
-module.exports.signuprouter=async(req,res)=>{
-    try{
-    let {username,email,password}=req.body;
-    let faker1=new user({
-    username,email});
-    let reg=await user.register(faker1,password);
-    console.log(reg);
-    req.login(reg,(err)=>{
-        if(err)
-        {
-            next(err);
-        }
-        req.flash("success", `Welcome ${username}!`);
-    res.redirect("/listings");
-    })
-    
-    }catch(e)
-    {
-        req.flash("error", `already existing ${username}!`);
-        console.log("EXISTING USERNAME OR EMAIL ID");
-        res.redirect("/signup");
+
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        // no need to create for username becoz passport-local-mongoose takes care of that
     }
-
 }
-
-
-module.exports.loginroute= async(req,res)=>{
-    let pg=await res.locals.reqpath||"/listings";
-res.redirect(pg);
-
-}
-
-
-module.exports.logoutroute=(req,res,next)=>{
-    req.logout((err)=>{
-        if(err)
-        {
-            next(err);
-        }
-        req.flash("success","successfully loggedout");
-        res.redirect("/listings");
-    })
-}
+)
+userSchema.plugin(passportlocalmongoose);
+const user=new mongoose.model("user",userSchema);
+module.exports=user;
